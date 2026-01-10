4914866cb71005bb4d9676728acebd39ee610fab
HopeIsAllwaysAlive <135110754+HopeIsAllwaysAlive@users.noreply.github.com>
2026-01-10 22:22:41 +0100

    Add files via upload

---
+++ pwa_service_worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'dicteer-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Installatie - cache bestanden
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activatie - verwijder oude caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - serveer van cache, valt terug op netwerk
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
